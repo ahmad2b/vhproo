f71a2a75b88c3c0f3560ceac83e66443c677568d
mr.javaria.y2b@gmail.com <javaria.y2b@gmail.com>
2024-07-18 23:02:44 +0500

    Services Section added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Hero() {
   return (
-    <section className="relative w-full h-[100vh] flex items-center justify-center px-4 lg:px-24">
+    <section className="relative w-full h-[70vh] md:h-[100vh] flex items-center justify-center px-4 lg:px-24">
       <img
         src="/washing.jpg"
         alt="Pressure Washing"
@@ -13,7 +13,7 @@ export default function Hero() {
           Professional Pressure Washing Services
         </h1>
         <p className="text-lg md:text-xl text-white drop-shadow-md">
-          Transform your property with our top-notch pressure washing solutions. We'll make your surfaces look brand new
+          Transform your property with our top-notch pressure washing solutions. We`&apos;`ll make your surfaces look brand new
           again.
         </p>
         <Link
